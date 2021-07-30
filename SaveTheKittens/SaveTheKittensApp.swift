dc742751a9bf1882b68226942d0eae7aabd41987
Bolu <bolu.oladun@gmail.com>
2021-07-30 15:21:22 +0100

    Initial Commit

---
+++ SaveTheKittens/SaveTheKittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensApp.swift
+//  SaveTheKittens
+//
+//  Created by Bolu Oladunjoye on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
